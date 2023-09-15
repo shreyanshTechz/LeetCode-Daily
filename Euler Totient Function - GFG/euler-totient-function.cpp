6ea2c5b9f8d46b17e80641f8e9850b100b166816
Shreyansh Pandey <85357239+shreyanshTechz@users.noreply.github.com>
2023-09-15 23:31:47 +0530

    Added solution - LeetHub

--- Euler Totient Function - GFG/euler-totient-function.cpp
+++ Euler Totient Function - GFG/euler-totient-function.cpp
@@ -7,17 +7,17 @@ class Solution{
 public:
     // function to find totient of n
     long long ETF(long long n){
-        vector<int> phi(n + 1);
-    for (int i = 0; i <= n; i++)
-        phi[i] = i;
- 
-    for (int i = 2; i <= n; i++) {
-        if (phi[i] == i) {
-            for (int j = i; j <= n; j += i)
-                phi[j] -= phi[j] / i;
+         int phi_n = n;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                while (n % i == 0)
+                    n /= i;
+                phi_n -= phi_n / i;
+            }
         }
-    }
-  return phi[n];
+        if (n > 1)
+            phi_n -= phi_n / n;
+        return phi_n;
     }
 };
 
